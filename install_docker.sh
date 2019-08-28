de3e1c676d53415649748052a2e247781514e80d
Rafael Peria de Sene <rpsene@br.ibm.com>
2019-08-28 14:08:18 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -35,7 +35,7 @@ apt-get install \
     software-properties-common -y
 
 # Add Docker GPG Key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 # Verify that you now have the key with the fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88
 apt-key fingerprint 0EBFCD88
